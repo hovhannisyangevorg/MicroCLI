4bd4e00a06c88ea315d851965bf6ac3c11e13c04
Gevorg <gev9911@gmail.com>
2023-09-09 12:37:08 +0400

    Hello Minishell )

---
+++ include/minishell.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gehovhan <gehovhan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 22:23:33 by gehovhan          #+#    #+#             */
+/*   Updated: 2023/09/08 22:25:11 by gehovhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# include "libft.h"
+# include "shell_struct.h"
+# include "shell_struct.h"
+
+/**
+ * @struct main.c
+ * @brief Represents settings for the screen dimensions and zoom.
+ */
+
+#endif
---
+++ include/shell_struct.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shell_struct.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gehovhan <gehovhan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 14:57:11 by gehovhan          #+#    #+#             */
+/*   Updated: 2023/09/08 22:24:43 by gehovhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHELL_STRUCT_H
+# define SHELL_STRUCT_H
+
+/**
+ * @struct s_name ?
+ * @brief Represents an image with associated properties.
+ */
+ 
+#endif
