d3763fd7c048effa1e2b4e6215af15c9534c8041
Armine Hakobyan <armhakob@c1r1s3.42yerevan.am>
2023-10-23 22:05:13 +0400

    dsadsa

---
+++ Libft/src/ft_strdup.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gevorg <gevorg@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 21:44:52 by gehovhan          #+#    #+#             */
+/*   Updated: 2023/09/29 17:48:14 by gevorg           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *str)
+{
+	char	*new;
+	size_t	len;
+	if (!str && !*str)
+		return (0);
+
+	len = ft_strlen(str) + 1;
+	new = (char *)malloc(sizeof(*new) * len);
+	if (!new)
+		return (NULL);
+	return ((char *)ft_memcpy(new, str, len));
+}
