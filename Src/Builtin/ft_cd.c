006e0697fdb72523269c36fe25008e749e21de8c
Hoso <hoso.hayrapetyan@gmail.com>
2024-02-12 22:00:06 +0400

    add builtins

--- Src/Builtin/ft_cd.c
+++ Src/Builtin/ft_cd.c
@@ -1,10 +1,47 @@
 #include "shell.h"
-
+#include <limits.h>
     
 int ft_cd(t_command* command, t_symbol_table* table)
 {
     (void)command;
     (void)table;
+    char *cwd;
+    char *oldcwd;
+
+
+    oldcwd = getcwd(NULL, 0);
+    if (command->argument && command->argument->arguments && command->argument->arguments[0])
+    {
+        if (!command->argument->arguments[1])
+        {
+            char* home = ft_get_env(table->env, "HOME");
+            if (!home)
+            {
+                ft_putendl_fd("cd: HOME is not set", STDERR_FILENO);
+                return 1;
+            }
+            else if (chdir(home) < 0)
+            {
+                perror("cd: ");
+                return errno;
+            }
+        }
+        else if (chdir(command->argument->arguments[1]) < 0)
+        {
+            perror("cd: ");
+            return errno;
+        }
+
+
+    }
+    cwd = getcwd(NULL, 0); 
+    if (!cwd)
+    {
+        perror("cd: ");
+        return errno;
+    }
+    ft_set_env(table->env, (t_hash_data){"OLDPWD", oldcwd, NORMAL});
+    ft_set_env(table->env, (t_hash_data){"PWD", cwd, NORMAL});
     return 0;
 
     
--- Src/Builtin/ft_unset.c
+++ Src/Builtin/ft_unset.c
@@ -1,41 +1,10 @@
 #include "shell.h"
 
-//ashxatox
-	
-// int	unset_helper(char **argv, t_env *my_env)
-// {
-
-// 	int i;
-// 	t_env *tmp;
-
-// 	i = 0;
-// 	tmp = my_env;
-// 	while (argv && argv[i++])
-// 	{
-// 		while (tmp != NULL)
-// 		{
-// 			if (ft_strcmp(argv[i], tmp->key) == 0)
-// 			{
-// 				tmp->hushir_indz = 1;
-// 				break; 
-// 			}
-// 			tmp = tmp->next;
-// 		}
-// 	}
-// 	return (0);
-// }
 
 int	ft_unset(t_command* command, t_symbol_table* table)
 {
-	(void)command;
-	(void)table;
-	ft_pop_entity(table->env, command->argument->arguments[1]);
+	if (command->argument && command->argument->arguments && command->argument->arguments[0] && command->argument->arguments[1])
+		ft_pop_entity(table->env, command->argument->arguments[1]);
+
 	return(0);
 }
-
-// int main(int argc, char **argv, char **env)
-// {
-// 	printf("%i", unset(argc, argv, env));
-// 	// printf("%i",env_m(argc, argv, env));
-// 	// printf ("%d", pwd(argc, argv, env));
-// }
