30eaa70601a753bce23b4437673ea523a1541ef2
Gevorg Hovhannisyan <gev9911@gmail.com>
2024-02-26 18:45:45 +0400

    norm

--- Include/Builtin/builtins.h
+++ Include/Builtin/builtins.h
@@ -1,55 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gevorg <gevorg@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 17:40:43 by gevorg            #+#    #+#             */
+/*   Updated: 2024/02/26 17:41:52 by gevorg           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
-# include "shell.h" 
+# include "shell.h"
 
-/**
- * Src/Builtin/ft_cd.c
- */
-int	ft_cd(t_command* command, t_symbol_table* table);
-
-/**
- * Src/Builtin/ft_echo.c
- */
-int ft_echo(t_command *command, t_symbol_table* table);
-
-/**
- * Src/Builtin/ft_env.c
- */
-int	ft_env(t_command* command, t_symbol_table* table);
-
-/**
- * Src/Builtin/ft_exit.c
- */
-int	ft_exit(t_command *command, t_symbol_table* table);
-
-/**
- * Src/Builtin/ft_export.c
- */
-int	ft_export(t_command* command, t_symbol_table* table);
-
-/**
- * Src/Builtin/ft_pwd.c
- */
-int	ft_pwd(t_command* command, t_symbol_table* table);
-
-
-/**
- * Src/Builtin/ft_unset.c
- */
-int	ft_unset(t_command* command, t_symbol_table* table);
-
-int	ft_history(t_command *command, t_symbol_table *table);
-
-
-int         ft_ismultiply_overflow_u(long a, long b);
-int         ft_isadd_overflow_u(long a, long b);
-size_t      ft_atoll_util(size_t i, char *str, int *is_overflowed);
-long long   ft_atoll(char *str, int *is_overflowed);
-
-
-int     ft_is_valid_identifier(char *ident);
-char	*ft_export_get_key(char *token, char *plus);
-char	*ft_export_collect_data(char *line, t_hash_data *data, char *plus);
+int			ft_cd(t_command *command, t_symbol_table *table);
+int			ft_echo(t_command *command, t_symbol_table *table);
+int			ft_env(t_command *command, t_symbol_table *table);
+int			ft_exit(t_command *command, t_symbol_table *table);
+int			ft_export(t_command *command, t_symbol_table *table);
+int			ft_pwd(t_command *command, t_symbol_table *table);
+int			ft_unset(t_command *command, t_symbol_table *table);
+int			ft_history(t_command *command, t_symbol_table *table);
+int			ft_ismultiply_overflow_u(long a, long b);
+int			ft_isadd_overflow_u(long a, long b);
+size_t		ft_atoll_util(size_t i, char *str, int *is_overflowed);
+long long	ft_atoll(char *str, int *is_overflowed);
+int			ft_is_valid_identifier(char *ident);
+char		*ft_export_get_key(char *token, char *plus);
+char		*ft_export_collect_data(char *line, t_hash_data *data, char *plus);
 
 #endif
--- Include/Executor/executor_struct.h
+++ Include/Executor/executor_struct.h
@@ -6,54 +6,39 @@
 /*   By: gevorg <gevorg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 16:54:56 by gevorg            #+#    #+#             */
-/*   Updated: 2024/02/18 16:54:57 by gevorg           ###   ########.fr       */
+/*   Updated: 2024/02/26 18:45:14 by gevorg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef 	EXECUTOR_STRUCT_H
-# define 	EXECUTOR_STRUCT_H
+#ifndef EXECUTOR_STRUCT_H
+# define EXECUTOR_STRUCT_H
 
-#include "parser_struct.h"
-#include "tokenizer_struct.h"
+# include "parser_struct.h"
+# include "tokenizer_struct.h"
 
+typedef int							(*t_function_callback)();
 
-typedef enum 	e_exec_type			t_exec_type;
-typedef struct	s_hash_entity		t_hash_entity;
-typedef struct	s_hash_entity_list	t_hash_entity_list;
-typedef struct	s_entity_table		t_entity_table;
-typedef enum	e_hash_type			t_hash_type;
-typedef struct 	s_hash_table 		t_hash_table;
-typedef struct 	s_vector			t_vector;
+typedef enum e_exec_type			t_exec_type;
+typedef enum e_visibility_type		t_visibility_type;
+typedef enum e_hash_type			t_hash_type;
+typedef enum e_expand_type			t_expand_type;
+typedef enum e_signal_type			t_signal_type;
 
-
-typedef int (*t_function_callback)();
-
-typedef struct 	s_function_entity       t_function_entity;
-
-typedef struct 	s_env_entity            t_env_entity;
-
-typedef struct 	s_hash_table_arr        t_hash_table_arr;
-
-typedef struct 	s_symbol_table          t_symbol_table;
-
-typedef enum 	e_visibility_type		t_visibility_type;
-
-typedef struct 	s_hash_data				t_hash_data;
-
-typedef enum 	e_expand_type			t_expand_type;
-
-typedef enum 	e_signal_type			t_signal_type;
-
-typedef struct s_process_info           t_process_info;
-typedef struct s_expand_token           t_expand_token;
-
-typedef struct s_file_mode              t_file_mode;
-
-typedef struct s_hash_iterate           t_hash_iterate;
-
-typedef struct s_history                t_history;
-
-
+typedef struct s_hash_entity		t_hash_entity;
+typedef struct s_hash_entity_list	t_hash_entity_list;
+typedef struct s_entity_table		t_entity_table;
+typedef struct s_hash_table			t_hash_table;
+typedef struct s_vector				t_vector;
+typedef struct s_function_entity	t_function_entity;
+typedef struct s_env_entity			t_env_entity;
+typedef struct s_hash_table_arr		t_hash_table_arr;
+typedef struct s_symbol_table		t_symbol_table;
+typedef struct s_hash_data			t_hash_data;
+typedef struct s_process_info		t_process_info;
+typedef struct s_expand_token		t_expand_token;
+typedef struct s_file_mode			t_file_mode;
+typedef struct s_hash_iterate		t_hash_iterate;
+typedef struct s_history			t_history;
 
 enum e_signal_type
 {
@@ -70,10 +55,10 @@ enum e_exec_type
 
 enum e_hash_type
 {
-    FUNCTION,
-    ENV
+	FUNCTION,
+	ENV
 };
- 
+
 enum e_expand_type
 {
 	EXPAND,
@@ -82,132 +67,121 @@ enum e_expand_type
 
 enum e_visibility_type
 {
-    NORMAL  = 1L << 0,
-    VENV    = 1L << 1,
-    EXPORT  = 1L << 3,
-    HIDDEN  = 1L << 2,
+	NORMAL	= 1L << 0,
+	VENV	= 1L << 1,
+	EXPORT	= 1L << 3,
+	HIDDEN	= 1L << 2,
 };
 
 struct s_hash_entity
 {
-    char			*key;
-    t_hash_entity	*next;
+	char				*key;
+	t_hash_entity		*next;
 };
 
 struct s_hash_entity_list
 {
-    t_hash_entity*  head;
-    t_hash_entity*  tail;
-    size_t          size;
+	t_hash_entity		*head;
+	t_hash_entity		*tail;
+	size_t				size;
 };
-
 
 struct s_entity_table
 {
-    t_hash_type         type;
-    t_hash_entity_list* entity;
-    size_t              size;
-    size_t              capacity;
+	t_hash_type			type;
+	t_hash_entity_list	*entity;
+	size_t				size;
+	size_t				capacity;
 };
 
 struct s_hash_table
 {
-	t_entity_table* table;
+	t_entity_table	*table;
 };
-
 
 struct s_hash_data
 {
-    char*               key;
-    void*               data;
-    t_visibility_type   type;
+	char				*key;
+	void				*data;
+	t_visibility_type	type;
 };
 
 struct s_function_entity
 {
-    t_hash_entity           base;
-    t_function_callback     function;
+	t_hash_entity		base;
+	t_function_callback	function;
 };
 
 struct s_env_entity
 {
-    t_hash_entity           base;
-    t_visibility_type       visibility;
-    char*                   value;
-
+	t_hash_entity		base;
+	t_visibility_type	visibility;
+	char				*value;
 };
-
 
 struct s_hash_table_arr
 {
-    char**  table;
-    size_t  size;
+	char				**table;
+	size_t				size;
 };
-
 
 struct s_symbol_table
 {
-  t_hash_table* env;
-  t_hash_table* function;  
+	t_hash_table		*env;
+	t_hash_table		*function;
 };
-
 
 struct s_vector
 {
-	int			*arr;
-	size_t		size;
-	size_t		capacity;
+	int					*arr;
+	size_t				size;
+	size_t				capacity;
 };
-
 
 struct s_process_info
 {
-    t_command           *command;
-    t_io                io;
-    t_symbol_table      *table;
-    t_vector            pipe_fd;
-    size_t              pipe_iter;
-    size_t              pipe_count;
-    int                 status;
-    t_hash_table_arr    env;
-    int                 is_builtin;
+	t_command			*command;
+	t_io				io;
+	t_symbol_table		*table;
+	t_vector			pipe_fd;
+	size_t				pipe_iter;
+	size_t				pipe_count;
+	int					status;
+	t_hash_table_arr	env;
+	int					is_builtin;
 };
 
 struct s_expand_token
 {
-    size_t  index_quot;
-    size_t  size;
-    size_t  index;
-    char    *for_rep;
-    char    *token;   
+	size_t				index_quot;
+	size_t				size;
+	size_t				index;
+	char				*for_rep;
+	char				*token;
 };
-
 
 struct s_file_mode
 {
-    int             access_mode;
-    int             open_mode;
-    t_token_type    mode;
-    int             is_heredoc;
-    int             is_input;
+	int					access_mode;
+	int					open_mode;
+	t_token_type		mode;
+	int					is_heredoc;
+	int					is_input;
 };
-
 
 struct s_hash_iterate
 {
-    int                 mode;
-    t_visibility_type   visibility;
-    t_hash_entity       *entity;
-    size_t              pos;
+	int					mode;
+	t_visibility_type	visibility;
+	t_hash_entity		*entity;
+	size_t				pos;
 };
 
 struct s_history
 {
-    char    **lines;
-    size_t  size;
-    size_t  capacity;
+	char				**lines;
+	size_t				size;
+	size_t				capacity;
 };
-
-
 
 #endif
